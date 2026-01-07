cbb24216b1b733569de6d64abe04d05683196ec1
Lohitha Rathipinni <lohitharathipinni27@gmail.com>
2026-01-07 17:18:57 +0530

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,11 @@
+public class Movie {
+    private String name;
+    private String genre;
+    private int duration;
+
+    public Movie(String name, String genre, int duration) {
+        this.name = name;
+        this.genre = genre;
+        this.duration = duration;
+    }
+}
